3659d1536257c8c30546593c674742a7111efbe1
Agung Tri A <agung.t.atmojo@gmail.com>
2020-06-04 17:48:15 +0700

    update main.dart

--- cupertino_store/lib/main.dart
+++ cupertino_store/lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:cupertino_store/model/app_state_model.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 import 'app.dart';
 
 void main() {
-  return runApp(CupertinoStoreApp());
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (_) => AppStateModel()..loadProducts(),
+      child: CupertinoStoreApp(),
+    )
+  );
 }
